7cfdbe574e68c746ff561e72c59ca30759783585
Mikhail Proniushkin <mikhailp@palantir.com>
2016-08-25 00:43:42 -0700

    basic fetching

--- frontend/app/pages/browse/index.js
+++ frontend/app/pages/browse/index.js
@@ -1,12 +1,36 @@
 import React from 'react'
 import Helmet from "react-helmet";
+import { connect } from 'react-redux'
+import { asyncConnect } from 'redux-connect'
+import { loadTracks } from '../../reducers'
 
+@asyncConnect([{
+  promise: ({ store }) => Promise.all([
+    store.getState().app.tracksLoaded ? Promise.resolve() : store.dispatch(loadTracks())
+  ])
+}], state => ({
+  tracks: state.app.tracks || []
+}))
 export default class Browse extends React.Component {
+
+	renderTrack(trackData) {
+		const id = trackData.id;
+    const name = trackData.name;
+
+    return (
+      <div key={id}>
+        <h1>{trackData.artist} - {trackData.name}</h1>
+      </div>
+    );
+  }
+
 	render() {
+		console.log(this.props.tracks)
 		return (
 			<div>
 			 	<Helmet title="Browse" />
 				Browse Tracks
+				{this.props.tracks.map(this.renderTrack)}
 			</div>
 		)
 	}
--- frontend/app/reducers/index.js
+++ frontend/app/reducers/index.js
@@ -4,6 +4,8 @@ import { reducer as formReducer } from 'redux-form'
 import { reducer as reduxAsyncConnect } from 'redux-connect'
 import _ from 'lodash'
 
+export const LOAD_TRACKS = 'app/LOAD_TRACKS'
+
 function checkStatus(response) {
   if (response.status >= 200 && response.status < 300) {
     return response
@@ -17,8 +19,40 @@ function parseJSON(response) {
   return response.json()
 }
 
+
+export function loadTracks() {
+  console.log('load shit')
+  return dispatch => {
+    dispatch({ type: LOAD_TRACKS })
+
+    try {
+      console.log('try')
+      return fetch('http://localhost:3002/tracks')
+        .then(checkStatus)
+        .then(parseJSON)
+        .then((data) => {
+          console.log('sccess')
+          dispatch({
+            type: `${LOAD_TRACKS}_SUCCESS`,
+            result: data
+          })
+        }).catch((error) => {
+          console.log('request to tracks failed', error)
+        })
+    } catch (err) {
+      console.log('Error', err)
+    }
+  }
+}
+
 const appReducer = (state = {}, action) => {
   switch (action.type) {
+    case `${LOAD_TRACKS}_SUCCESS`:
+      console.log('success switch')
+      return Object.assign({}, state, {
+        tracks: action.result,
+        tracksLoaded: true
+      })
     default:
       return state
   }
