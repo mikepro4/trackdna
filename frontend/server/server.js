35e1043ac21b47cfcfc360a458059133c7ea677b
Mikhail Proniushkin <mikhailp@palantir.com>
2016-08-26 15:31:19 -0700

    deploy

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -42,80 +42,11 @@ app.get('*',(req, res) => {
   const store = finalCreateStore(reducers)
 
   match({ history, routes, location: req.originalUrl }, (error, redirectLocation, renderProps) => {
-    if (error) {
-      res.status(500).send(error.message)
-    } else if (redirectLocation) {
-      res.redirect(302, redirectLocation.pathname + redirectLocation.search)
-    } else if (renderProps) {
-      if (__DEV__) {
-        global.webpack_isomorphic_tools.refresh()
-      }
 
-      const loadData = process.env.SERVER_LOAD_DATA
-        ? loadOnServer(Object.assign({}, renderProps, { store }))
-        : Promise.resolve(1)
-      loadData.then(() => {
-        let serverRender = ''
-        let serverState = ''
-        let stylesheets = []
-        let inlineStyles = []
-
-        let bundleScriptFile
-        let headTitle
-        const assets = global.webpack_isomorphic_tools.assets()
-
-        if (process.env.SSR === 'true') {
-          // You can also check renderProps.components or renderProps.routes for
-          // your "not found" component or route respectively, and send a 404 as
-          // below, if you're using a catch-all route.
-          serverRender = renderToString(
-            <Provider store={store}>
-              <ReduxAsyncConnect {...renderProps} />
-            </Provider>
-          )
-          let head = Helmet.rewind();
-          headTitle = head.title.toString()
-          /* styles (will be present only in production with webpack extract text plugin) */
-          stylesheets = Object.keys(assets.styles).map((style) =>
-            `<link href='${assets.styles[style]}' media="screen, projection" rel="stylesheet" type="text/css"/>`)
-
-          inlineStyles = Object.keys(assets.assets)
-            .map(k => assets.assets[k])
-            .filter(v => v._style)
-            .map(v => `<style>${v._style}</style>`)
-
-          serverState = `<script charSet='UTF-8'>window.__data=${serialize(store.getState())}</script>`
-          bundleScriptFile = `<script src=${assets.javascript.main} charSet='UTF-8'></script>`
-        } else {
-          bundleScriptFile = `<script src=${assets.javascript.main} charSet='UTF-8'></script>`
-        }
-
-        const finalStylesheets = stylesheets.join('\r\n')
-        const finalInlineStyles = inlineStyles.join('\r\n')
-
-        res.set('content-type', 'text/html')
-        // IMPORTANT – Website is rendering through this function
-        res.status(200).send(template({
-          headTitle,
-          serverRender,
-          serverState,
-          bundleScriptFile,
-          finalStylesheets,
-          finalInlineStyles
-        }))
-        res.end()
+        res.status(200).send('Hi')
       })
-    } else {
-      res.status(404).send('Not found')
-    }
-  })
 })
 
-app.listen(process.env.PORT, 'localhost', function (err) {
-  if (err) {
-    console.log(err)
-    return
-  }
+app.listen(3000, 'localhost', function (err) {
 
-  console.log(`Listening at http://localhost:${process.env.PORT}`)
 })
