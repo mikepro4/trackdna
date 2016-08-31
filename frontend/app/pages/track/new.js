df10dff0c2eeae2a78eb82c3bb4d60588c28ee78
Mikhail Proniushkin <mikhailp@palantir.com>
2016-08-30 19:54:06 -0700

    fuck i love urls

--- frontend/app/pages/track/new.js
+++ frontend/app/pages/track/new.js
@@ -26,6 +26,9 @@ export default class TrackNew extends React.Component {
     router: PropTypes.object
   };
 
+	componentWillMount() {
+	}
+
   handleFormSubmit({ artist, name}) {
     this.props.dispatch(addTrack({
         artist,
@@ -45,45 +48,33 @@ export default class TrackNew extends React.Component {
     this.props.dispatch(updateSearchTerm(artist, track_name))
     this.props.dispatch(searchYoutube(`${artist} ${track_name}`))
     this.props.dispatch(searchBeatport(`${artist} ${track_name}`))
+    this.context.router.push(`/track/new?artistName=${encodeURI(artist)}&trackName=${encodeURI(track_name)}`);
   }
 
   componentWillUnmount() {
     this.props.dispatch(clearSearchResults())
   }
 
+  componentDidMount() {
+    console.log(this.props.location.query)
+    if(this.props.location.query.artistName && this.props.location.query.trackName && !this.props.search.videos) {
+      const artist = this.props.location.query.artistName
+      const track_name = this.props.location.query.trackName
+      this.handleTrackSearchSubmit({artist, track_name})
+    }
+  }
+
   render() {
-    // const oauth = OAuth({
-    //     consumer: {
-    //         public: 'ac24c59a7d307088bde16c1954710b3a1be86959',
-    //         secret: 'c517c8175fc5bbfb50adb202ebad7db8c014fcae'
-    //     },
-    //     signature_method: 'HMAC-SHA1'
-    // });
-    //
-    // const token = {
-    //     public: '44544de2454973ae18248c12ae5ad70093c03158',
-    //     secret: 'b7728c4a3cd249b170fb43e285875d98226f1792'
-    // };
-    //
-    // const request_data = {
-    //   url: 'https://oauth-api.beatport.com/catalog/3/most-popular/genre?id=6&perPage=100',
-    //   method: 'GET'
-    // };
-
-    // axios.get('https://oauth-api.beatport.com/catalog/3/most-popular/genre?id=6&perPage=100', {headers: oauth.toHeader(oauth.authorize(request_data, token))})
-    //   .then((results) => console.log(results.data.results))
-    //
-    // YTSearch({key: 'AIzaSyDQ_kgowJCa-mH5wnjnQ1mOE4nBqQIGij8', term:'Moon Rocks Enrico Sangiuliano'}, (videos) => {
-    //   console.log(videos)
-    //  });
-    //
-    // axios.get('https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics&id=VG5OqG_DTVo&key=AIzaSyDQ_kgowJCa-mH5wnjnQ1mOE4nBqQIGij8')
-    //  .then((results) => console.log(results.data.items[0].statistics))
-
+    const initialState = {
+      initialValues: {
+        track_name: this.props.location.query.trackName,
+        artist: this.props.location.query.artistName
+      }
+    }
     return (
       <div className='page_container page_add_track'>
         <Helmet title="New Track – Track DNA" />
-        <TrackSearchForm {...this.props} onSubmit={this.handleTrackSearchSubmit.bind(this)} />
+        <TrackSearchForm {...this.props} {...initialState} onSubmit={this.handleTrackSearchSubmit.bind(this)} />
         <TrackSearchResults {...this.props} />
       </div>
     );
