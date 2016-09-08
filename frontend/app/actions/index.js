4a423d6fdb69d5f737bdca7c67431a294c484bc0
Mikhail Proniushkin <mikhailp@palantir.com>
2016-09-08 16:25:00 -0700

    omg that's bad

--- frontend/app/actions/index.js
+++ frontend/app/actions/index.js
@@ -15,7 +15,7 @@ import {
   UPDATE_TIME
 } from './types';
 
-const ROOT_URL = 'http://localhost:3002';
+const ROOT_URL = 'http://192.241.217.109:3002';
 
 export function updateCurrentVideo(id, action, seconds) {
   return {
