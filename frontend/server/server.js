7e2cb82a201a6f3c8ddb6325c4121cd62000ec8f
Mikhail Proniushkin <mikhailp@palantir.com>
2016-09-08 16:44:32 -0700

    check for dev

--- frontend/server/server.js
+++ frontend/server/server.js
@@ -120,13 +120,15 @@ app.listen(process.env.PORT, function (err) {
   console.log(`Listening at http://localhost:${process.env.PORT}`)
 })
 
-var jsonServer = require('json-server')
-var server = jsonServer.create()
-var router = jsonServer.router('data/db.json')
-var middlewares = jsonServer.defaults()
+if (!global.__DEV__) {
+  var jsonServer = require('json-server')
+  var server = jsonServer.create()
+  var router = jsonServer.router('data/db.json')
+  var middlewares = jsonServer.defaults()
 
-server.use(middlewares)
-server.use(router)
-server.listen(3002, function () {
-  console.log('JSON Server is running')
-})
+  server.use(middlewares)
+  server.use(router)
+  server.listen(3002, function () {
+    console.log('JSON Server is running')
+  })
+}
