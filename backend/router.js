a38d647c93fd59ace8a5081a6bcbb9f48286f404
avertsel <alex.vertsel@gmail.com>
2016-09-16 08:46:22 +0300

    another test commit

--- backend/router.js
+++ backend/router.js
@@ -4,6 +4,8 @@ const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
+// importing passport
+
 import axios from 'axios';
 // importing axios
 
