602e7df5521a648a4ba512e4c9bed6f9eabefac0
avertsel <alex.vertsel@gmail.com>
2016-09-18 23:37:43 +0300

    Youtube and Beatport APIs endpoints added.

---
+++ backend/controllers/external_search.js
@@ -0,0 +1,101 @@
+import axios from 'axios';
+import OAuth from 'oauth-1.0a'
+import ytdl from 'ytdl-core'
+import exec from 'child_process';
+import fs from 'fs';
+
+const YOUTUBE_API_KEY = 'AIzaSyDQ_kgowJCa-mH5wnjnQ1mOE4nBqQIGij8'
+const oauth = OAuth({
+    consumer: {
+        public: 'ac24c59a7d307088bde16c1954710b3a1be86959',
+        secret: 'c517c8175fc5bbfb50adb202ebad7db8c014fcae'
+    },
+    signature_method: 'HMAC-SHA1'
+});
+const token = {
+    public: '44544de2454973ae18248c12ae5ad70093c03158',
+    secret: 'b7728c4a3cd249b170fb43e285875d98226f1792'
+};
+const youtubeDefaultResultsLimit = 10;
+const beatportDefaultResultsLimit = 10;
+
+export function searchYoutube(req, res, next) {
+  var searchTerm = req.body.searchTerm;
+  var resultsLimit = req.body.resultsLimit != undefined ?
+    req.body.resultsLimit : youtubeDefaultResultsLimit;
+  console.log('search youtube for: ', searchTerm);
+  const searchTermEncoded = encodeURIComponent(searchTerm);
+  return axios.get(`https://www.googleapis.com/youtube/v3/search?part=snippet&key=${YOUTUBE_API_KEY}&q=${searchTermEncoded}&type=video&maxResults=${resultsLimit}`)
+    .then(function (response) {
+        res.json({ videos: response.data.items });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+export function loadYoutubeVideoData(req, res, next) {
+  var videoId = req.params.id;
+  console.log('load youtube data: ', videoId)
+  return axios.get(`https://www.googleapis.com/youtube/v3/videos?part=contentDetails,statistics&id=${videoId}&key=${YOUTUBE_API_KEY}`)
+    .then(function (response) {
+         res.json({videoYoutubeDetails: response.data.items[0]})
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+}
+
+export function loadYoutubeWave(req, res, next) {
+  var videoId = req.params.id;
+  console.log('load youtube wave: ', videoId);
+  var url = `http://www.youtube.com/watch?v=${videoId}`;
+  var base = __dirname + "/../../frontend/static";
+  var flvFile = `${base}/flv/video_${videoId}.flv`;
+  var pngFile = `${base}/wave/wave_${videoId}.png`;
+  if (fs.existsSync(pngFile)) {
+    console.log(`Wave already there: ${pngFile}`);
+    res.json({ waveUrl: `/wave/wave_${videoId}.png` });
+    return;
+  }
+
+  var stream = ytdl(url);
+  stream.on('end', function () {
+    console.log(`Download completed: flv ${videoId}`);
+    // the next command is for older version of mmpeg installed on DO
+    // exec.exec(`ffmpeg -i ${flvFile} -lavfi showwavespic=split_channels=0:s=1024x800 ${pngFile}`);
+
+    var command = 'ffmpeg -i ' + flvFile + ' -filter_complex "[0:a]aformat=channel_layouts=mono, \
+     compand=gain=-6, \
+     showwavespic=s=1000x120:colors=#555555[fg]; color=s=1000x120:color=#ffffff, \
+     drawgrid=width=iw/10:height=ih/5:color=#000000@0.1[bg]; \
+     [bg][fg]overlay=format=rgb" -vframes 1 ' + pngFile
+    exec.exec(command);
+
+    res.json({ waveUrl: `/wave/wave_${videoId}.png` });
+  });
+  stream.pipe(fs.createWriteStream(flvFile));
+}
+
+export function searchBeatport(req, res, next) {
+  var searchTerm = req.body.searchTerm;
+  var resultsLimit = req.body.resultsLimit != undefined ?
+    req.body.resultsLimit : beatportDefaultResultsLimit;
+  console.log('search beatport for: ', searchTerm);
+  const searchTermEncoded = encodeURIComponent(searchTerm);
+  const url = `https://oauth-api.beatport.com/catalog/3/search/?query=${searchTermEncoded}&perPage=${resultsLimit}`;
+  const request_data = {
+    url: url,
+    method: 'GET'
+  };
+
+  return axios.get(url, {
+    headers: oauth.toHeader(oauth.authorize(request_data, token))
+  })
+  .then(function (response) {
+    res.json({ videos: response.data.results });
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+}
--- backend/router.js
+++ backend/router.js
@@ -4,10 +4,7 @@ const passport = require('passport');
 
 const requireAuth = passport.authenticate('jwt', { session: false });
 const requireSignin = passport.authenticate('local', { session: false });
-// importing passport
-
-import axios from 'axios';
-// importing axios
+const externalSearch = require('./controllers/external_search');
 
 module.exports = function(app) {
   app.get('/', requireAuth, function(req, res) {
@@ -15,4 +12,8 @@ module.exports = function(app) {
   });
   app.post('/signin', requireSignin, Authentication.signin);
   app.post('/signup', Authentication.signup);
+  app.post('/api/youtube/search', externalSearch.searchYoutube);
+  app.get('/api/youtube/data/:id', externalSearch.loadYoutubeVideoData);
+  app.get('/api/youtube/wave/:id', externalSearch.loadYoutubeWave);
+  app.post('/api/beatport/search', externalSearch.searchBeatport);
 }
